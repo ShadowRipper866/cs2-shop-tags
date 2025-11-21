d32f0d4f6dc1233af0f09d5410890bc62eb7dabe
ShadowRipper866 <bahlsen700@gmail.com>
2025-11-21 14:05:54 +0200

    Remove old method

--- main.h
+++ main.h
@@ -37,7 +37,6 @@ public:
     bool Load(PluginId id, ISmmAPI* ismm, char* error, size_t maxlen, bool late);
     bool Unload(char* error, size_t maxlen);
     void AllPluginsLoaded();
-    void FireGameEvent(IGameEvent *pEvent);
 private:
     const char* GetAuthor();
     const char* GetName();
